8afc3247b4a3f0ddaefe51cd03a067e81d254261
Julian Fajardo <jafajardo37@misena.edu.co>
2018-12-11 16:57:20 -0500

    Chore: se sube la publicacion del diagrama de clases y las interfaces HTML

---
+++ 02. Diseño/08. HTML/Proyecto1566614/js/gmap.js
@@ -0,0 +1,36 @@
+var mapLocation = new google.maps.LatLng(4.596239, -74.112075); //change coordinates here
+var marker;
+var map;
+
+function initialize() {
+    var mapOptions = {
+        zoom: 18, //change zoom here
+        center: mapLocation
+    };
+    
+    map = new google.maps.Map(document.getElementById('map-canvas'), 
+    mapOptions);
+    
+    var image = 'img/flag.png';
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        title: 'Site Name', //change title here
+        icon: image,
+        animation: google.maps.Animation.DROP,
+        animation: google.maps.Animation.BOUNCE,
+        position: mapLocation
+    });
+    google.maps.event.addListener(marker, 'click', toggleBounce);
+}
+
+function toggleBounce() {
+    
+    if (marker.getAnimation() != null) {
+        marker.setAnimation(null);
+    } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
