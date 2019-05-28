68e832595dfcfc305c2cae9b728343470a42d075
Julian Fajardo <jafajardo37@misena.edu.co>
2019-05-28 08:14:30 -0500

    Chore: se añade un slidea la pag principal

--- 03.Desarrollo/02.CodigoFuente/tecnica/V1.0.0/src/main/webapp/app/modules/home/home.tsx
+++ 03.Desarrollo/02.CodigoFuente/tecnica/V1.0.0/src/main/webapp/app/modules/home/home.tsx
@@ -4,12 +4,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Translate } from 'react-jhipster';
 import { connect } from 'react-redux';
-import { Row, Col, Alert } from 'reactstrap';
+import { Row, Col, Alert, UncontrolledCarousel } from 'reactstrap';
 
 import { IRootState } from 'app/shared/reducers';
 import { getSession } from 'app/shared/reducers/authentication';
 
 export interface IHomeProp extends StateProps, DispatchProps {}
+const items = [
+  {
+    src: 'http://3.bp.blogspot.com/-zrmw9YeU3nw/Wg2wTcYeYaI/AAAAAAAADhI/XlG-aH_ScsQc4vGWqw4p-qbddsVBvky2gCK4BGAYYCw/s1600/DSC03985.JPG',
+    altText: 'Slide 1',
+    caption: 'Slide 1'
+  },
+  {
+    src:
+      'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa20%20text%20%7B%20fill%3A%23444%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa20%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23666%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22247.3203125%22%20y%3D%22218.3%22%3ESecond%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
+    altText: 'Slide 2',
+    caption: 'Slide 2'
+  },
+  {
+    src:
+      'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa21%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa21%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277%22%20y%3D%22218.3%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
+    altText: 'Slide 3',
+    caption: 'Slide 3'
+  }
+];
 
 export class Home extends React.Component<IHomeProp> {
   componentDidMount() {
@@ -20,7 +39,7 @@ export class Home extends React.Component<IHomeProp> {
     const { account } = this.props;
     return (
       <Row>
-        <Col md="9">
+        <Col md="7">
           <h2>
             <Translate contentKey="home.title">Welcome, Java Hipster!</Translate>
           </h2>
@@ -50,31 +69,33 @@ export class Home extends React.Component<IHomeProp> {
               </Alert>
 
               <Alert color="warning">
-                <Translate contentKey="global.messages.info.register.noaccount">You do not have an account yet?</Translate>&nbsp;
+                <Translate contentKey="global.messages.info.register.noaccount">You do not have an account yet?</Translate>
+                &nbsp;
                 <Link to="/register" className="alert-link">
                   <Translate contentKey="global.messages.info.register.link">Register a new account</Translate>
                 </Link>
               </Alert>
             </div>
           )}
-              <h2>
-                <Translate contentKey="home.link.homepage">JHipster homepage</Translate>
-              </h2>
-              <p>
-                <Translate contentKey="home.link.chat">JHipster public chat room</Translate>
-              </p>
-              <p>
-                <Translate contentKey="home.link.follow">follow @java_hipster on Twitter</Translate>
-              </p>
+          <h2>
+            <Translate contentKey="home.link.homepage">JHipster homepage</Translate>
+          </h2>
+          <p>
+            <Translate contentKey="home.link.chat">JHipster public chat room</Translate>
+          </p>
+          <p>
+            <Translate contentKey="home.link.follow">follow @java_hipster on Twitter</Translate>
+          </p>
           <p>
             <Translate contentKey="home.like">If you like JHipster, do not forget to give us a star on</Translate>{' '}
             <a href="https://github.com/SENA-CEET/D.W.B.H-CORPORATION" target="_blank" rel="noopener noreferrer">
               Github
-            </a>!
+            </a>
+            !
           </p>
         </Col>
-        <Col md="3" className="pad">
-          <span className="hipster rounded" />
+        <Col md="5" className="pad">
+          <UncontrolledCarousel items={items} />
         </Col>
       </Row>
     );
